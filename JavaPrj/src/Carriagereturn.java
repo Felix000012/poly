ec97826a29aa3be0fe0ba3f80d2488c96d239bf6
Felix000012 <80371899+Felix000012@users.noreply.github.com>
2021-03-17 16:19:08 +0900

    Add files via upload

---
+++ JavaPrj/src/Carriagereturn.java
@@ -0,0 +1,11 @@
+
+public class Carriagereturn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("123\r");
+		System.out.println("123\r");
+		System.out.println("123\r");
+	}
+
+}
---
+++ JavaPrj/src/SpecialCharEx.java
@@ -0,0 +1,14 @@
+
+public class SpecialCharEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println('\'');
+		System.out.println("abc\t123\b456");
+		System.out.println("123\f123\n123\r123\\123\'\''");
+		System.out.println("123\r");
+		System.out.println("123\\");
+		System.out.println("123\f)");
+	}
+
+3/17
