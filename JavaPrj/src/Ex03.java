c328ba8b60c25a850199836c38034d1f617464a0
Felix000012 <80371899+Felix000012@users.noreply.github.com>
2021-03-17 16:19:42 +0900

    Add files via upload

---
+++ JavaPrj/src/Ex03.java
@@ -0,0 +1,25 @@
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a, b, c, d, e;
+
+		a = 'A';
+		System.out.printf(" %c \t ", a);
+		System.out.printf(" %d \n", (int) a);
+
+		b = 'a';
+		c = (char) (b + 1);
+		System.out.printf(" %c \t", b);
+		System.out.printf(" %c \n", c);
+
+		d = 90;
+		System.out.printf(" %C \n", d);
+
+		d = '°¡';
+		e = (char) (d + 1);
+		System.out.printf(" %d \t", (int) d);
+		System.out.printf(" %c \n", e);
+	}
+}
