c6a8fe1c9de45b1e76208c4a69ed81bee3634dc9
Felix000012 <80371899+Felix000012@users.noreply.github.com>
2021-03-23 18:21:57 +0900

    Add files via upload

---
+++ JavaPrj/src/OperatorEx1718.java
@@ -0,0 +1,12 @@
+
+public class OperatorEx1718 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	double pi = 3.141592;
+	double shortPi = (int)(pi*1000+0.5)/1000.0; //반올람울 하기위해 0.5를 더해준 것이다.
+	System.out.println(shortPi);
+	
+	}
+}
---
+++ JavaPrj/src/OperatorEx3.java
@@ -0,0 +1,12 @@
+
+public class OperatorEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i = 5, j = 5;
+System.out.println(i++);
+System.out.println(++j); //j를 i로 바꾸면 i=7 이 나온다
+System.out.println("i = " + i + ", j = " +j);
+	}
+
+}
