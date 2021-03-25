40244e58660e4b78b3e69b0d808269e7dbc37c1f
Felix000012 <80371899+Felix000012@users.noreply.github.com>
2021-03-25 19:28:49 +0900

    Add files via upload

---
+++ JavaPrj/src/OperatorEx27.java
@@ -0,0 +1,15 @@
+
+public class OperatorEx27 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean b = true;
+		char ch = 'C';
+
+		System.out.printf("ch=%c%n", ch);
+		System.out.printf("ch < 'a' || ch> 'z' = %b%n", ch < 'a' || ch > 'z');
+		System.out.printf("!('a'<=ch && ch <='z')=%b%n", !('a' <= ch && ch <= 'z'));
+		System.out.printf(" 'a' <=ch && ch<='z' =%b%n", 'a' <= ch && ch <= 'z');
+	}
+
+}
