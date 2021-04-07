e4c3c8a1238703e42be266839e595583d1ee5396
Felix000012 <80371899+Felix000012@users.noreply.github.com>
2021-04-07 23:38:06 +0900

    Add files via upload

---
+++ onetomili.java
@@ -0,0 +1,12 @@
+
+public class onetomili {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				int d = 0;
+				while (d < 1000) {
+					d++;
+					System.out.println(d + " ");
+				}
+			}
+		}
