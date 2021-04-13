9ad5e867efcb2eb10505dceb3a8e1cf90b0f17e7
Felix000012 <80371899+Felix000012@users.noreply.github.com>
2021-04-13 23:46:24 +0900

    Add files via upload

---
+++ rec.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class rec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("한 변의 길이를 정해주세요.");
+		int a;
+		a = s.nextInt();
+
+		
+		for (a = 1; a <= 10; a++) {
+
+			for (a = 1; a <= 10; a++) {
+				System.out.print("*");
+		}
+	System.out.println();}
+	}
+
+}
