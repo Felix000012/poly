28f43374e89862e9973ce0ce2bcb752db5b663bc
Felix000012 <80371899+Felix000012@users.noreply.github.com>
2021-04-15 23:46:40 +0900

    Add files via upload

---
+++ tm6.java
@@ -0,0 +1,14 @@
+
+public class tm6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		int i;
+		for (i = 3; i < 9; i++) {
+			sum += i;
+			System.out.println(sum);
+	}
+
+	}
+}
